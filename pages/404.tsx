0ec7caaa854161d316672fcfe13fe94675992b1b
Sergio Barria <sergio.barria9210@gmail.com>
2021-07-17 16:50:20 -0500

    add not found page

---
+++ pages/404.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+import { FaExclamationTriangle } from 'react-icons/fa';
+
+import Layout from '../components/Layout';
+import styles from '../styles/404.module.css';
+
+const NotFoundPage: React.FC = () => {
+  return (
+    <Layout title='Page not Found'>
+      <div className={styles.error}>
+        <h1>
+          <FaExclamationTriangle />
+          404
+        </h1>
+        <h4>Sorry, there is nothing here</h4>
+        <Link href='/'>Go Back Home</Link>
+      </div>
+    </Layout>
+  );
+};
+
+export default NotFoundPage;
