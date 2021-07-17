a900fbde0bb845983053cfd3b2a3057835306c72
Sergio Barria <sergio.barria9210@gmail.com>
2021-07-17 16:53:22 -0500

    add paths aliases

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head';
 
 import Header from './Header';
 import Footer from './Footer';
-import styles from '../styles/Layout.module.css';
+import styles from '@/styles/Layout.module.css';
 
 interface LayoutProps {
   title?: string;
